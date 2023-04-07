c8105ccfce6d58b0c2189b35151d3436ec247454
nico-vrn <networksgamesni@gmail.com>
2023-04-07 10:13:45 +0200

    fav

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ function rechercher(){ //fonction de recherche
     favoris();
 }
 
+//gestion de la map
 
 var map = null;
 function initMap(lat,lon) { //fonction d'initialisation de la carte
@@ -72,4 +73,152 @@ function initMap(lat,lon) { //fonction d'initialisation de la carte
         // Nous définissons à quelle carte il est ajouté
         map: map
     });
+}
+
+
+//gestion des favoris
+
+favoris();
+document.addEventListener('keyup', function(e) {favoris();});
+
+function favoris(){ //gestion des favoris
+  if(localStorage.fav==undefined){ //si pas de localStorage
+    localStorage.fav=JSON.stringify({favoris:[]});
+  }
+  var loupe=document.getElementById("btn-favoris");
+  var fav=recup_fav();
+  var elmt_fav=fav.favoris;
+  if (elmt_fav.length==0){ //si pas de favoris
+    etoile("vide");
+    loupe.addEventListener("click",ajouter_fav);
+    return;
+  }
+  else{ //si des favoris
+    for (var i=0;i<fav.favoris.length;i++){
+      if (elmt_fav[i]==document.getElementById("champs_recherche").value){ //si le champ de recherche est déjà dans les favoris
+        etoile("pleine");
+        loupe.removeEventListener("click",ajouter_fav);
+        loupe.addEventListener("click",function(){
+          supprimer_fav("etoile");
+        });
+        return;
+      }
+      else{ //si le champ de recherche n'est pas dans les favoris
+        etoile("vide");
+        loupe.removeEventListener("click",supprimer_fav);
+        loupe.addEventListener("click",ajouter_fav);
+      }
+    }
+  }
+}
+
+function recup_fav(){ //recupère les favoris
+  var fav=JSON.parse(localStorage.fav);
+  return fav;
+}
+
+function etoile(cote){  //affiche les étoiles
+  var loupe=document.getElementById("btn-favoris");
+  var img_pleine=document.getElementById("img-pleine");
+  var img_vide=document.getElementById("img-vide");
+  if(cote=="pleine"){ //si pleine
+    img_pleine.style.display="block";
+    img_vide.style.display="none";
+    loupe.style.backgroundColor="var(--main-green)";
+    loupe.style.border=".1em solid grey"
+  }
+  else{   //si vide
+    loupe.style.backgroundColor="grey";
+    img_pleine.style.display="none";
+    img_vide.style.display="block";
+  }
+}
+
+function ajouter_fav(){ //ajoute un favoris
+  recherche=document.getElementById("champs_recherche").value;
+  empty=document.getElementById('empty');
+  if(recherche==""){ //si champs de recherche vide
+    empty.textContent="Vous ne pouvez pas ajouter rien au favoris";
+  }
+  else{ //si champs de recherche rempli
+    empty.textContent="";
+    var fav=recup_fav();
+    fav.favoris.push(recherche);
+    localStorage.fav=JSON.stringify(fav);
+    favoris();
+    affiche_fav();
+  }
+}
+
+function supprimer_fav(cmt){ //supprime un favoris
+  //console.log("supprimer_fav");
+  var fav=recup_fav();
+  var elmt_fav=fav.favoris;
+  favoris();
+  if (cmt=="etoile"){ //si clic sur étoile
+    var elmt_fav_supp=document.getElementById("champs_recherche").value;
+  }
+  else{ //si clic sur le bouton supprimer
+    var elmt_fav_supp=cmt;
+  }
+  for (var i=0;i<fav.favoris.length;i++){ //pour chaque favoris
+    if (elmt_fav[i]==elmt_fav_supp){ //si le favoris à supprimer est le favoris en cours
+      if(confirm("êtes vous sur de vouloire supprimer le favoris ?")){
+        fav.favoris.splice(i,1);
+        localStorage.fav=JSON.stringify(fav);
+        favoris()
+        affiche_fav();
+        return;
+      }
+    }
+    }
+}
+
+function affiche_fav(){ //affiche les favoris
+  clear_fav(); //efface les favoris
+  var liste=document.getElementById("liste-favoris");
+  var empty=document.getElementById("info-vide");
+  var btn_supp=document.getElementById("btn-supp");
+  var fav = recup_fav();
+  if (fav.favoris.length==0){ //si pas de favoris
+    empty.style.display="block";
+    btn_supp.style.display="none";
+  }
+  else{ //si des favoris
+    empty.style.display="none";
+    btn_supp.style.display="block";
+    btn_supp.addEventListener("click",function(){
+      localStorage.clear();
+      favoris();
+      affiche_fav();
+    });
+    for (var i=0;i<fav.favoris.length;i++){ //pour chaque favoris
+      newLi=document.createElement("li");
+      newA=document.createElement("a");
+      newA.textContent=fav.favoris[i];
+      newA.addEventListener("click",function(){
+        document.getElementById("champs_recherche").value=this.textContent;
+        rechercher();
+      });
+      var img_croix=document.createElement("img");
+      img_croix.src="images/croix.svg";
+      img_croix.title="Supprimer le favoris";
+      img_croix.width="22";
+      img_croix.setAttribute('id',fav.favoris[i])
+      img_croix.addEventListener("click",function(){
+        var ID=this.getAttribute('id');
+        supprimer_fav(ID);
+      });
+      newLi.appendChild(newA);
+      newLi.appendChild(img_croix);
+      liste.appendChild(newLi);
+    }
+  }
+}
+
+function clear_fav(){ //efface les favoris
+  var liste=document.getElementById("liste-favoris");
+    while (liste.firstChild) {
+        liste.removeChild(liste.firstChild);
+    }
 }
