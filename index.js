2620e0b948218bea209661408c36e8281ae516b0
nico-vrn <networksgamesni@gmail.com>
2023-04-07 10:12:22 +0200

    map

--- index.js
+++ index.js
@@ -39,3 +39,37 @@ function rechercher(){ //fonction de recherche
     favoris();
 }
 
+
+var map = null;
+function initMap(lat,lon) { //fonction d'initialisation de la carte
+    // Créer l'objet "map" et l'insèrer dans l'élément HTML qui a l'ID "map"
+	map = new google.maps.Map(document.getElementById("map"), {
+        // Nous plaçons le centre de la carte avec les coordonnées ci-dessus
+        center: new google.maps.LatLng(lat, lon),
+        // Nous définissons le zoom par défaut
+        zoom: 12,
+        // Nous définissons le type de carte (ici carte routière)
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        // Nous activons les options de contrôle de la carte (plan, satellite...)
+        mapTypeControl: true,
+        // Nous désactivons la roulette de souris
+        scrollwheel: false,
+        mapTypeControlOptions: {
+            // Cette option sert à définir comment les options se placent
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR
+        },
+        // Activation des options de navigation dans la carte (zoom...)
+        navigationControl: true,
+        navigationControlOptions: {
+            // Comment ces options doivent-elles s'afficher
+            style: google.maps.NavigationControlStyle.ZOOM_PAN
+        }
+
+    });// Nous ajoutons un marqueur
+    var marker = new google.maps.Marker({
+        // Nous définissons sa position (syntaxe json)
+        position: {lat: lat, lng: lon},
+        // Nous définissons à quelle carte il est ajouté
+        map: map
+    });
+}
