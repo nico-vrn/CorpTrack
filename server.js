deb3bb234fc01f7e27caa679544c3b9db1799ba4
nico-vrn <networksgamesni@gmail.com>
2023-04-07 13:17:20 +0200

    reorganisation v2

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const dotenv = require('dotenv');
-dotenv.config();
-
 const express = require('express');
+
+dotenv.config();
 const path = require('path');
 const app = express();
-
 const port = 3000;
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    shodanKey: process.env.shodanKey
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
