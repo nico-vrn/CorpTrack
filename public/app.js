918b97b93d44f2844b14a5fe7b7d0bc5ea53eae8
nico-vrn <networksgamesni@gmail.com>
2023-04-11 10:42:51 +0200

    up

--- public/app.js
+++ public/app.js
@@ -1,5 +1,3 @@
-const shodanKey = ""
-
 var bt_search=document.getElementById("btn-lancer-recherche");
 bt_search.addEventListener("click",rechercher);
 ch_search=document.getElementById("champs_recherche");
@@ -30,9 +28,6 @@ function afficher_IP(data){ //affiche l'IP de l'utilisateur avec un lien de rech
 function rechercher(){ //fonction de recherche
     console.log("recherche en cours");
     recherche=document.getElementById("champs_recherche").value;
-    var url = "https://api.shodan.io/shodan/host/search?" + shodanKey + "&query=" + recherche + "&facets=country";
-    var url2="https://api.shodan.io/shodan/host/search?key=${shodanKey}&query=country:france&facets=country"
-    var url3="https://api.shodan.io/shodan/host/count?key=${shodanKey}&query=port:22&facets=org,os"; // fonctionne retourne le nb de host par pays
     if(recherche[0]==undefined){ //si la recherche est vide
       document.getElementById('empty').textContent="le champs de recherche est vide";
     }
